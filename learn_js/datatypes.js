cd7102422c6b2776e4fca2cbee1809a1a5848573
Arbaz khan <99495106+Arbaz011@users.noreply.github.com>
2024-08-22 09:37:38 +0000

    datatypes

---
+++ learn_js/datatypes.js
@@ -0,0 +1,16 @@
+let number=null
+console.log(typeof number);
+console.log(number)
+let number2=true
+console.log(typeof number2);
+console.log(number2)
+let sym="@sym"
+console.log(typeof sym);
+console.log(sym)
+let a;
+console.log(typeof a);
+console.log(a)
+let num=787
+console.log(typeof num);
+console.log(num)
+let bigint=2
