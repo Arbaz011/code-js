52ccd7127743a605289778320b70e8377c03e6e9
Arbaz khan <99495106+Arbaz011@users.noreply.github.com>
2024-08-23 19:43:03 +0000

    Math fun

---
+++ learn_js/numandmath.js
@@ -0,0 +1,45 @@
+// math obect
+// E: 2.718281828459045;
+// LN2: 0.6931471805599453;
+// LN10: 2.302585092994046;
+// LOG2E: 1.4426950408889634;
+// LOG10E: 0.4342944819032518;
+// PI: 3.141592653589793;
+// SQRT1_2: 0.7071067811865476;
+// SQRT2: 1.4142135623730951;
+// const a=-9
+// const b=9
+// const c=99.55
+// console.log(Math.abs(a),Math.abs(b)) ; // both value will positive abs(absract) it change neg to +
+// console.log("PI value is ", Math.PI);
+// console.log("PI value in round off is " ,Math.round(Math.PI));
+// console.log(Math.sqrt(2));// it  will give sqaure root vale
+// console.log(Math.sqrt(144));
+// console.log(Math.max(2,3,5,9,1));// it will give maximum no
+// console.log(Math.min(2,3,5,9,1));//it will give minimum no
+
+// let k=99
+// console.log(k.toFixed(2));// it will give 99.00
+// console.log(Math.acos(-1));// it give pi value
+console.log(Math.ceil(87.995));//it give nearest roundoff
+console.log(Math.floor(8.8));//downward value on base of x,y axis
+console.log(Math.floor(-.8));//it give upperward valvebase of x,y axis
+console.log(Math.trunc(-.8));// -0
+console.log(Math.trunc(9.02));//only give integer
+console.log(Math.cbrt(8));//cube root
+console.log(Math.pow(2,3));// 2 ki power 3 so ans 8
+console.log(Math.random());// give random numb
+console.log(Math.clz32(1));//returns the number of leading zero bits in the 32-bit binary representation of a given number.
+console.log(Math.clz32(1000000000000000000000000000000000000));
+
+
+
+
+
+
+
+
+
+
+
+
