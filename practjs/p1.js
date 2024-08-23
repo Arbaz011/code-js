86e947cce182536a93aacac05ee652c18791de57
Arbaz khan <99495106+Arbaz011@users.noreply.github.com>
2024-08-23 13:05:55 +0000

    dict,practice

---
+++ practjs/p1.js
@@ -0,0 +1,28 @@
+// ---------------practice--------------
+// let a="arbaz"
+// let b=786
+// console.log(a+b);
+// let typeA=typeof a
+// let typeB=typeof b
+// console.log(`The  data type of a is ${typeA}. The data Type of b is ${typeB}.`)
+// --------------------2--------------------- 
+// const arbaz={
+//  home:"mallah",
+//  college:"ds",
+//  bike:"scooty"    
+// }
+// arbaz.car="bmw"
+// arbaz.college="AMU"
+// console.log(arbaz); to change propertise value in objet &or add propertise
+// ---------------------3---------------------------
+//  making dictionary
+const dict={
+    Compendium : "SummaryUsage",
+    Ruse :" Trick, deceptionUsage",
+    Clannish : "ExclusiveUsage",
+    Titular : "Having a title",
+    Satanic :" DemonicSentence"
+
+}
+console.log(dict.Compendium);
+console.log(dict.Ruse);
