2573817bd9d8009a4e38069ee1c60238f91f9f83
Arbaz khan <99495106+Arbaz011@users.noreply.github.com>
2024-08-23 10:58:10 +0000

    string

--- learn_js/datatypes.js
+++ learn_js/datatypes.js
@@ -1,20 +1,28 @@
-let number=null
-console.log(typeof number);
-console.log(number)
-let number2=true
-console.log(typeof number2);
-console.log(number2)
-let sym="@sym"
-console.log(typeof sym);
-console.log(sym)
-let a;
-console.log(typeof a);
-console.log(a)
-let num=787
-console.log(typeof num);
-console.log(num)
-console.log(bigIntLiteral)
-let bigIntLiteral = 9007199254740991n;
-console.log(typeof bigIntLiteral);
+// let number=null
+// console.log(typeof number);
+// console.log(number)
+// let number2=true
+// console.log(typeof number2);
+// console.log(number2)
+// let sym="@sym"
+// console.log(typeof sym);
+// console.log(sym)
+// let a;
+// console.log(typeof a);
+// console.log(a)
+// let num=787
+// console.log(typeof num);
+// console.log(num)
+// console.log(bigIntLiteral)
+// let bigIntLiteral = 9007199254740991n;
+// console.log(typeof bigIntLiteral);
 
-
+// non primitive 
+const arbaz= {
+    insta: "ak0989",
+    number: 948785585,
+salary: 80000,
+degree: "BCA"
+}
+console.log(arbaz["salary"]);
+console.log(arbaz["salary"]);
---
+++ learn_js/string.js
@@ -0,0 +1,17 @@
+// let a="hello"
+// console.log(a);
+// //also 
+// let a1='arbaz'
+// console.log(a1);
+// //by backlit `
+// console.log(`${a} bro how are u ${a1}`);
+// //also like object
+// let string=new String("a string  object")
+// console.log(string.length) // index length of string
+// console.log(string);
+
+
+
+
+
+
