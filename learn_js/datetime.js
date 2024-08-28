58bdc8551675052377068b7dee810fab6b3789e8
Arbaz khan <99495106+Arbaz011@users.noreply.github.com>
2024-08-28 18:48:20 +0000

    learn date and time

---
+++ learn_js/datetime.js
@@ -0,0 +1,23 @@
+// ----date and time in js
+// let todaydate= new Date();
+//  console.log(todaydate.toLocaleString());//   8/28/2024, 5:14:33 PM
+//  console.log(todaydate.toDateString());//    Wed Aug 28 2024
+//  console.log(todaydate.toString());//Wed Aug 28 2024 17:18:32 GMT+0000 (Coordinated Universal Time)
+//  console.log(todaydate.toTimeString());//17:20:02 GMT+0000 only time
+//  console.log(todaydate.toUTCString());//Wed, 28 Aug 2024 17:22:42 GMT
+//  console.log(todaydate.toISOString());//2024-08-28T17:25:57.665Z
+//  console.log(todaydate.toJSON());//2024-08-28T17:31:23.973Z
+//  console.log(todaydate.toLocaleDateString())//8/28/2024
+//  console.log(typeof todaydate)// object
+
+
+// let knowday=new Date(2003,2,21)
+// console.log(knowday.toDateString()) // Fri Mar 21 2003 we get that day was friday
+
+let mydate=new Date("2003-03-21")
+console.log(mydate);
+console.log(mydate.getDay());// value 5 ---mean fri
+console.log()
+
+
+
